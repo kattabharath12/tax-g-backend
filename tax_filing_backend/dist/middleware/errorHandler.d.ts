4de8fc19052def511d54377f62a96470ca96e655
kattabharath12 <106069483+kattabharath12@users.noreply.github.com>
2025-07-27 12:29:57 -0500

    Delete tax_filing_backend directory

--- tax_filing_backend/dist/middleware/errorHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export interface AppError extends Error {
-    statusCode?: number;
-    isOperational?: boolean;
-}
-export declare const errorHandler: (err: AppError, req: Request, res: Response, next: NextFunction) => void;
-export declare const asyncHandler: (fn: Function) => (req: Request, res: Response, next: NextFunction) => Promise<any>;
-//# sourceMappingURL=errorHandler.d.ts.map
